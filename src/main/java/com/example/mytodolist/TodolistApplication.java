18bd7bda9c007ffb6956587214be5b1c2caceab1
William Le <william7e.se@gmail.com>
2023-01-11 16:30:42 +0100

    update

--- src/main/java/com/example/mytodolist/TodolistApplication.java
+++ src/main/java/com/example/mytodolist/TodolistApplication.java
@@ -4,10 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TodolistApplication {
 
+
+	@GetMapping("/")
+	public String welcomeMessage(){
+		return " Welcome to my site";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
